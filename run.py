41e4ef8fb56977ff2cc5c2e07310b2806f3dde18
resloved <bfriesenwork@gmail.com>
2022-06-09 12:39:04 -0700

    Programmatically infer from run

--- run.py
+++ run.py
@@ -8,26 +8,10 @@ import copy
 import torch
 from torch.nn import functional as F
 
+
 from src.model import RWKV_RNN
 
 np.set_printoptions(precision=4, suppress=True, linewidth=200)
-
-print('''
-******************************************************************************
-* This is a preview of RWKV-v2-RNN trained on the Pile for only 50B tokens.
-* It is NOT indicative of the final performance (which requires 300B tokens).
-******************************************************************************''')
-
-# Edit src/model.py to set CPU / CUDA mode. Runs on CPU by default.
-
-TEMPERATURE = 1.0
-TOP_P = 0.7
-
-DEBUG_DEBUG = False
-LENGTH_OF_EACH = 333
-NUM_TRIALS = 100
-
-context = '\nIn a shocking finding, scientist discovered a herd of dragons living in a remote, previously unexplored valley, in Tibet. Even more surprising to the researchers was the fact that the dragons spoke perfect Chinese.'
 
 ##############################################################################################################
 
@@ -48,43 +32,54 @@ def sample_logits(out, temperature=1.0, top_p=None):
     return torch.multinomial(probs, num_samples=1)[0]
 
 
-for TRIAL in range(1 if DEBUG_DEBUG else NUM_TRIALS):
-    ctx = [model.tokenizer.encode(context)][0]
-    src_len = len(ctx)
-    print(context, end='')
+def infer(
+    prompt,
+    trials=1,
+    length_min=1,
+    length_max=333,
+    seperators=[".", "!", "?", "\n"],
+    temperature=1.0,
+    top_p=0.7,
+):
+    results = ["" for x in range(trials)]
+    for trial in range(trials):
+        ctx = [model.tokenizer.encode(prompt)][0]
+        src_len = len(ctx)
+        model.clear()
 
-    model.clear()
-    if TRIAL == 0:
-        init_state = types.SimpleNamespace()
-        for i in range(src_len if DEBUG_DEBUG else src_len):
-            x = ctx[:i+1]
-            if i == src_len - 1:
-                init_state.out = model.run(x)
-            else:
-                model.run(x)
-        model.save(init_state)
-    else:
-        model.load(init_state)
-
-    if DEBUG_DEBUG:
-        out = init_state.out
-        print('\n', np.array(x), '==>', np.array(
-            out), np.max(out), np.min(out))
-
-    for i in range(src_len, src_len + (0 if DEBUG_DEBUG else LENGTH_OF_EACH)):
-        x = ctx[:i+1]
-        x = x[-model.ctx_len:]
-
-        if i == src_len:
-            out = copy.deepcopy(init_state.out)
+        if trial == 0:
+            init_state = types.SimpleNamespace()
+            for i in range(src_len):
+                x = ctx[: i + 1]
+                if i == src_len - 1:
+                    init_state.out = model.run(x)
+                else:
+                    model.run(x)
+                    model.save(init_state)
         else:
-            out = model.run(x)
+            model.load(init_state)
 
-        out[0] = -999999999  # disable <|endoftext|>
+        out = init_state.out
+        for i in range(src_len, src_len + length_max):
+            x = ctx[: i + 1]
+            x = x[-model.ctx_len :]
 
-        char = sample_logits(out, temperature=TEMPERATURE, top_p=TOP_P)
-        char = char.item()
-        print(model.tokenizer.decode(char), end='', flush=True)
+            if i == src_len:
+                out = copy.deepcopy(init_state.out)
+            else:
+                out = model.run(x)
 
-        ctx += [char]
-    print('\n' + '-' * 70, end='')
+                out[0] = -999999999  # disable <|endoftext|>
+
+                char = sample_logits(out, temperature=temperature, top_p=top_p)
+                char = char.item()
+                decoded = model.tokenizer.decode(char)
+                results[trial] += decoded
+                if (
+                    seperators
+                    and src_len + length_min < i
+                    and any(sep in decoded for sep in seperators)
+                ):
+                    break
+                ctx += [char]
+    return results
